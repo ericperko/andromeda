4ac3592c29d0b74f5c872f765fddb9d63c9917bf
Eric Perko <wisesage5001@gmail.com>
2010-07-19 02:42:19 -0400

    Initial work on the UKF... should be able to generate sigma points now

--- sparkfun_9dof_razor/src/imu_broadcaster.py
+++ sparkfun_9dof_razor/src/imu_broadcaster.py
@@ -9,42 +9,45 @@ from sparkfun_9dof_razor.msg import State
 import serial
 
 class SF9DOF_Broadcaster:
-	def __init__(self, port, magneto_err, gyro_err, accel_err):
-		self.port = port
-		self.mag_err = magneto_err
-		self.gyro_err = gyro_err
-		self.accel_err = accel_err
-		self.pub = rospy.Publisher("state", State)
-		self.message = State()
-		self.message.header.frame_id = "sf9dof_link"
+    def __init__(self, port, magneto_err, gyro_err, accel_err):
+        self.port = port
+        self.mag_err = magneto_err
+        self.gyro_err = gyro_err
+        self.accel_err = accel_err
+        self.pub = rospy.Publisher("state", State)
+        self.message = State()
+        self.message.header.frame_id = "sf9dof_link"
+        self.message.mag_error = self.mag_err
+        self.message.omega_error = self.gyro_err
+        self.message.accel_error = self.accel_err
 
-	def loop(self):
-		while not rospy.is_shutdown():
-		    line = port.readline()
-		    chunks = line.split(":")
-		    if chunks[0] == "!IMU":
-			    readings = chunks[1].split(",")
-			    self.message.header.stamp = rospy.Time.now()
-			    self.message.magnetometer.x = float(readings[0])
-			    self.message.magnetometer.y = float(readings[1])
-			    self.message.magnetometer.z = float(readings[2])
-			    self.message.angular_velocity.x = float(readings[3])
-			    self.message.angular_velocity.y = float(readings[4])
-			    self.message.angular_velocity.z = float(readings[5])
-			    self.message.linear_acceleration.x = float(readings[6])
-			    self.message.linear_acceleration.y = float(readings[7])
-			    self.message.linear_acceleration.z = float(readings[8])
-			    self.pub.publish(self.message)
-		    else:
-			    rospy.logerr("Did not get a valid IMU packet, got %s", line)
+    def loop(self):
+        while not rospy.is_shutdown():
+            line = port.readline()
+            chunks = line.split(":")
+            if chunks[0] == "!IMU":
+                readings = chunks[1].split(",")
+                self.message.header.stamp = rospy.Time.now()
+                self.message.magnetometer.x = float(readings[0])
+                self.message.magnetometer.y = float(readings[1])
+                self.message.magnetometer.z = float(readings[2])
+                self.message.angular_velocity.x = float(readings[3])
+                self.message.angular_velocity.y = float(readings[4])
+                self.message.angular_velocity.z = float(readings[5])
+                self.message.linear_acceleration.x = float(readings[6])
+                self.message.linear_acceleration.y = float(readings[7])
+                self.message.linear_acceleration.z = float(readings[8])
+                self.pub.publish(self.message)
+            else:
+                rospy.logerr("Did not get a valid IMU packet, got %s", line)
 
 if __name__ == "__main__":
-	rospy.init_node('sparkfun_9dof_razor')
-	port_name = rospy.get_param("~port_name", "/dev/ttyUSB0")
-	port = serial.Serial(port_name, 115200, timeout = 1)
-	magneto_error = rospy.get_param("~magneto_error", 0.1)
-	gyro_error = rospy.get_param("~gyro_error", 0.1)
-	accel_error = rospy.get_param("~accel_error", 0.1)
-	broadcaster = SF9DOF_Broadcaster(port, magneto_error, gyro_error, accel_error)
-	broadcaster.loop()
-	port.close()
+    rospy.init_node('sparkfun_9dof_razor')
+    port_name = rospy.get_param("~port_name", "/dev/ttyUSB0")
+    port = serial.Serial(port_name, 115200, timeout = 1)
+    magneto_error = rospy.get_param("~magneto_error", 0.1)
+    gyro_error = rospy.get_param("~gyro_error", 0.1)
+    accel_error = rospy.get_param("~accel_error", 0.1)
+    broadcaster = SF9DOF_Broadcaster(port, magneto_error, gyro_error, accel_error)
+    broadcaster.loop()
+    port.close()
---
+++ sparkfun_9dof_razor/src/imu_ukf.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+import roslib
+roslib.load_manifest("sparkfun_9dof_razor")
+import rospy
+
+from sparkfun_9dof_razor.msg import State
+from sensor_msgs.msg import Imu
+
+from numpy import *
+import numpy.linalg
+
+class SF9DOF_UKF:
+    def __init__(self):
+        self.pub = rospy.Publisher("imu_estimate", Imu)
+        self.is_initialized = False
+        self.beta = rospy.get_param("~beta", 2)
+        self.alpha = rospy.get_param("~alpha", 2)
+        self.kappa = rospy.get_param("~kappa", 1.5)
+        self.n = 10
+        self.kf_lambda = pow(self.alpha,2.) * (self.n + self.kappa) - self.n
+        self.weight_covariance = ones(self.n * 2 + 1)
+        self.weight_mean = ones(self.n * 2 + 1)
+        self.weight_mean = self.weight_mean * (1. / (2 * (self.n +
+            self.kf_lambda)))
+        self.weight_covariance = self.weight_covariance * (1. / (2 * (self.n +
+            self.kf_lambda)))
+        self.weight_mean[0] = self.kf_lambda / (self.n + self.kf_lambda)
+        self.weight_covariance[0] = self.kf_lambda / (self.n + self.kf_lambda) + (1 -
+                pow(self.alpha, 2) + self.beta)
+        self.initialize_filter()
+
+    def initialize_filter(self):
+        self.is_initialized = False
+        self.time = rospy.Time.now()
+        self.kalman_state = zeros((self.n,1))
+        self.kalman_covariance = diag(ones(self.kalman_state.shape[0]))
+        self.kalman_state[3,0] = 1.0
+        self.is_initialized = True
+
+    def handle_measurement(self, measurement):
+        if not self.is_initialized:
+            rospy.logwarn("Filter is unintialized. Discarding measurement")
+        else:
+            pass
+
+    def generate_sigma_points(self, mean, covariance):
+        sigmas = []
+        sigmas.append(mean)
+        temp = (self.n + self.kf_lambda) * covariance
+        temp = numpy.linalg.cholesky(temp)
+        for i in range(0,self.n):
+            #Must use columns in order to get the write thing out of the
+            #Cholesky decomposition
+            #(http://en.wikipedia.org/wiki/Cholesky_decomposition#Kalman_filters)
+            sigmas.append(mean + temp[:,i].reshape(10,1))
+            sigmas.append(mean - temp[:,i].reshape(10,1))
+        return sigmas
+
+if __name__ == "__main__":
+    rospy.init_node("sf9dof_ukf")
+    ukf = SF9DOF_UKF()
+    rospy.Subscriber("state", State, ukf.handle_measurement)
+    rospy.spin()
